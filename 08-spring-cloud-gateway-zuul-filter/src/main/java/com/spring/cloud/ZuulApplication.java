cf79e953121055e0eec7a1a62d365871d22d7bae
刘义 <liuyi@daling.com>
2019-08-20 20:51:30 +0800

    添加zuul网关支持

---
+++ 08-spring-cloud-gateway-zuul-filter/src/main/java/com/spring/cloud/ZuulApplication.java
@@ -0,0 +1,23 @@
+package com.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author liuyi
+ * @date 2019/8/20
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+
+    @Bean
+    public PreRequestLogFilter preRequestLogFilter() {
+        return new PreRequestLogFilter();
+    }
+}
